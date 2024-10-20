e5ca6b54493ba27634c41905b9f0dabdc48a4ca7
Paulo Medeiros <paulo.medeiros@myopentobuy.com.br>
2024-10-20 20:03:09 -0300

    Added tre nem endpoints.

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/UserController.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/UserController.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 //CREAT, READ, UPDATE E DELETE.
 namespace CadastroDeUsuario.Controllers
 {
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        [HttpPost("CreatUser")]
+        public string creatUser(string name, string telefone, string endereço, string cpf)
         {
-            return View();
+            if (string.IsNullOrEmpty(name))
+            {
+                return "O nome informado e invalido";
+            }
+            return "Usuario cadastrado";
         }
-        
-        
+
+
+        // metodo = endpoint
+
+
+        [HttpGet("GetAllUsers")]
+        public List<string> GetAllUsers()
+        {
+            var allUser = new List<string>();
+            var user1 = "Caio";
+            var user2 = "Bode";
+            var user3 = "Paulo";
+            var user4 = "Akamaru";
+
+            allUser.Add(user1);
+            allUser.Add(user2);
+            allUser.Add(user3);
+            allUser.Add(user4);
+
+            return allUser;
+        }
+
+        [HttpGet("GetUserByName")]
+        public List<string> GetUserByName(string name)
+        {
+        }
     }
 }
