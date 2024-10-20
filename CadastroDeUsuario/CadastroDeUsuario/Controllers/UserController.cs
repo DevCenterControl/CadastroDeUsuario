1e297fc40a69c35bf534a3e3f2af7ce46b7847de
Lucas Oliveira <lucasboliveira.96@outlook.com>
2024-10-20 17:52:48 -0300

    Add new UserController.

---
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeUsuario.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
