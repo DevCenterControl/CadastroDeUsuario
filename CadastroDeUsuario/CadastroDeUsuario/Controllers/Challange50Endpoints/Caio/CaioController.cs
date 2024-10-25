3dd1625badde28e231cca68209cec59bf817ff05
Caio Rodrigo <caiorodrigo_dev@outlook.com>
2024-10-25 19:17:34 -0300

    Adding YourAge enpoint in CaioController.

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/Challange50Endpoints/Caio/CaioController.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/Challange50Endpoints/Caio/CaioController.cs
@@ -444,5 +444,21 @@ namespace CadastroDeUsuario.Controllers.Challange.Caio
                 return false;
             }
         }
+
+        public class HowOldAreYou
+        {
+            [Required(ErrorMessage = "Your age is required.")]
+            [Range(1, 100, ErrorMessage = "Your age must be between 1 and 100 years old.")]
+            public required int Age { get; set; }
+        }
+        [HttpPost("YourAge")]
+        public IActionResult YourAge(HowOldAreYou number)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok($"You are {number.Age} years old");
+        }
     }
 }
