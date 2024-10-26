f775a53ee6475d33406bbd62ba55e6d7a8102fb9
Paulo Medeiros <paulo.medeiros@myopentobuy.com.br>
2024-10-25 22:24:33 -0300

    validações basicas no email, explicação.

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/AuthController.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/AuthController.cs
@@ -6,14 +6,35 @@ namespace CadastroDeUsuario.Controllers
     [ApiController]
     public class AuthController : Controller
     {
+        string[] provedoresPermitidos = { "@gmail.com", "@outlook.com", "@hotmail.com", "@yahoo.com" };
+
         [HttpPost("Login")]
-        public bool Login(string user, string password)
+        public string Login(string email, string password)
         {
-            if (user == "Paulo" && password == "123")
+            if (string.IsNullOrEmpty(email))
             {
-                return true;
+                return "Email não pode estar em branco";
             }
-            return false;   
+
+            if (email.Length <= 10)
+            {
+                return "Email inválido.";
+            }
+
+            if (!email.Contains("@"))
+            {
+                return "Email invalido";
+            }
+
+            if (email.Contains("@@"))
+            {
+                return "Email invalido";
+            }
+
+           var isPermitted = provedoresPermitidos.Any(provedor => email.EndsWith(provedor, StringComparison.OrdinalIgnoreCase));
+
+
+
         }
     }
 }
