e2f359c9ea63357f91e88267df910bc7b434798e
Caio Rodrigo <caiorodrigo_dev@outlook.com>
2024-10-30 19:13:38 -0300

    Adding LoginRequest Model .

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/ModelsAuthController/LoginRequest.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/ModelsAuthController/LoginRequest.cs
@@ -1,8 +1,17 @@
-﻿namespace CadastroDeUsuario.Controllers.ModelsAuthController
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CadastroDeUsuario.Controllers.ModelsAuthController
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage = "Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        [MaxLength(50, ErrorMessage = "Limite de caracteres atingido, seu limite é de 50 caracteres.")]
         public string email { get; set; }
+
+        [Required(ErrorMessage = "Password é obrigatorio.")]
+        [MinLength(8, ErrorMessage ="Password deve conter no minimo 8 caracteres.")]
         public string password { get; set; }
     }
 }
