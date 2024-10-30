423997754f1f6944684eba5446a3ec24f6d9d894
Caio Rodrigo <caiorodrigo_dev@outlook.com>
2024-10-30 19:44:35 -0300

    New folder adding ModelsAuthController and adding models in AuthController.

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/AuthController.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/AuthController.cs
@@ -12,102 +12,108 @@ namespace CadastroDeUsuario.Controllers
         string specialCharacters = "!@#$%&*(){}[];:.,<>?/-=+¢¬£~^`_¨|";
 
         [HttpPost("Login")]
-        public string Login(string email, string password)
+        public IActionResult Login([FromBody] LoginRequest request)
         {
-            if (string.IsNullOrEmpty(email))
+            if (!ModelState.IsValid)
             {
-                return "Email não pode estar em branco";
+                return BadRequest (ModelState);
             }
+
+            string email = request.email;
+            string password = request.password;
 
             if (email.Length <= 10)
             {
-                return "Email inválido.";
+                return BadRequest("Email inválido.");
             }
 
-            if (!email.Contains("@"))
-            {
-                return "Email invalido";
-            }
+            //if (!email.Contains("@"))
+            //{
+            //    return "Email invalido";
+            //}
+            //Obs: Validação do @ agora é feita pelo model utilizando EmailAdress,
+            //que verifica os requisitos minimos para um email ser valido.
 
             if (email.Contains("@@"))
             {
-                return "Email invalido";
+                return BadRequest("Email invalido");
             }
 
             var isPermitted = provedoresPermitidos.Any(provedor => email.EndsWith(provedor, StringComparison.OrdinalIgnoreCase));
 
-            if (isPermitted == false)
+            if (!isPermitted)
             {
-                return "Email com provedor invalido";
+                return BadRequest("Email com provedor invalido");
             }
 
             if (email.Contains("!"))
             {
-                return "Caracteres Invalidos";
+                return BadRequest("Caracteres Invalidos");
             }
 
             if (email.Contains("#"))
             {
-                return "Caracteres Invalidos";
+                return BadRequest("Caracteres Invalidos");
             }
 
             if (email.Contains("$"))
             {
-                return "Caracteres Inválidos.";
+                return BadRequest("Caracteres Inválidos.");
             }
 
             if (email.Length >= 50)
             {
-                return "Limite de caracteres atingido, seu limite é de 50 caracteres.";
+                return BadRequest("Limite de caracteres atingido, seu limite é de 50 caracteres.");
             }
 
             if (email.Contains(".."))
             {
-                return "Email invalido, não é possivel utilizar dois pontos finais consecutivos (..).";
+                return BadRequest("Email invalido, não é possivel utilizar dois pontos finais consecutivos (..).")  ;
             }
 
             if (email.StartsWith("."))
             {
-                return "Email inválido, nao é possivel iniciar com ponto final. ";
+                return BadRequest("Email inválido, nao é possivel iniciar com ponto final. ");
             }
 
-            if (string.IsNullOrEmpty(password))
-            {
-                return "Password não pode estar em branco";
-            }
+            //////if (string.IsNullOrEmpty(password))
+            //////{
+            //////    return "Password não pode estar em branco";
+            //////} 
 
 
-            if (password.Length <= 8)
-            {
-                return "Password deve conter no minimo 8 caracteres.";
-            }
+            ////if (password.Length <= 8)
+            ////{
+            ////    return "Password deve conter no minimo 8 caracteres.";
+            //} Obs: Com a utilização das models esta validação nao é mais necessaria nesta parte do codigo,
+            //ele esta importando da classe LoginRequest os mesmos requisitos utilizando Required e MinLenght.
 
             if (password.Contains(email))
             {
-                return "O password nao pode ser igual ao email.";
+                return BadRequest("O password nao pode ser igual ao email.");
             }
 
             if (!password.Any(char.IsUpper))
             {
-                return "Password deve conter pelo menos uma letra maiuscula.";
+                return BadRequest ("Password deve conter pelo menos uma letra maiuscula.");
             }
 
             if (!password.Any(char.IsNumber))
             {
-                return "Passoword deve conter pelo menos um numero.";
+                return BadRequest("Passoword deve conter pelo menos um numero.");
             }
 
             if (!password.Any(ch => specialCharacters.Contains(ch)))
             {
-                return "Passowrd deve conter pelo menos um caractere especial.";
+                return BadRequest("Passowrd deve conter pelo menos um caractere especial.");
             }
 
             if (!password.Any(char.IsLower))
             {
-                return "Passowrd deve conter pelo menos uma letra minuscula.";
+                return BadRequest("Passowrd deve conter pelo menos uma letra minuscula.");
             }
 
-            return "Autenticado com sucesso.";
+            return Ok("Autenticado com sucesso.");
 
         }
 
