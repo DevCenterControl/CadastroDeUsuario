f44541616e1e3d1cb8e7e1fa766c0510154420a3
Paulo Medeiros <paulo.medeiros@myopentobuy.com.br>
2024-10-31 00:34:58 -0300

    Adicionando clausula where e explicando como funciona.

--- CadastroDeUsuario/CadastroDeUsuario/Controllers/UserController.cs
+++ CadastroDeUsuario/CadastroDeUsuario/Controllers/UserController.cs
@@ -26,6 +26,37 @@ namespace CadastroDeUsuario.Controllers
             }
         }
 
+        [HttpGet("GetUserByName")]
+        public List<string> GetUserByName(string name)
+        {
+            try
+            {
+
+                //essa é a nossa lista fake que esta simulando o banco de dados. (tipo uma lista telefonica das antigas)
+                var listaDeUsuario = new List<string>();
+                var usuario1 = "Paulo";
+                var usuario2 = "Lucas";
+                var usuario3 = "Caio";
+                var usuario4 = "Greg";
+                var usuario5 = "Jimmy";
+
+                listaDeUsuario.Add(usuario1);
+                listaDeUsuario.Add(usuario2);
+                listaDeUsuario.Add(usuario3);
+                listaDeUsuario.Add(usuario4);
+                listaDeUsuario.Add(usuario5);
+
+                var usuarioEncontrado = listaDeUsuario.Where(x => x.Contains(name)).ToList();
+
+                return usuarioEncontrado;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
 
         //Create user (meio caminho andado).
         //Get user by name.
