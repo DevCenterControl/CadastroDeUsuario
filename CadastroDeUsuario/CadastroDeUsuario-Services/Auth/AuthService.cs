2bf204c68c6643eccc774b339458738448db96f2
Paulo Medeiros <paulo.medeiros@myopentobuy.com.br>
2024-11-16 23:15:13 -0300

    Adding IAuthService.

---
+++ CadastroDeUsuario/CadastroDeUsuario-Services/Auth/AuthService.cs
@@ -0,0 +1,20 @@
+﻿using CadastroDeUsuario_DTO.Request.Auth;
+using CadastroDeUsuario_DTO.Response.Auth;
+using CadastroDeUsuario_Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeUsuario_Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        public AuthLoginResponseDTO Authenticate(AuthLoginRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CadastroDeUsuario/CadastroDeUsuario-Services/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using CadastroDeUsuario_DTO.Request.Auth;
+using CadastroDeUsuario_DTO.Response.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeUsuario_Services.Interfaces
+{
+    public interface IAuthService
+    {
+        AuthLoginResponseDTO Authenticate(AuthLoginRequestDTO request);
+
+    }
+}
