43ef224388a9875c22d7a9c3d623185493cc4427
Paulo Medeiros <paulo.medeiros@myopentobuy.com.br>
2024-12-17 10:05:59 -0300

    Add pacotes EF vinculado ao banco de dados.

---
+++ CadastroDeUsuario/CadastroDeUsuario-Domain/Entities/User/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CadastroDeUsuario_Domain.Entities.User
+{
+    public class User
+    {
+        public string Nome { get; set; } 
+     
+        public string Cpf { get; set; } 
+
+        public string Endereco { get; set; } 
+    }
+}
